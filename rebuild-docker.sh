5a9cece82d1b6e277fe063d4990b04283df3504d
MidKnight <romain.bouguet@epitech.eu>
2022-02-03 20:28:00 +0100

    adding script to rebuild docker containers + deleting adminer

---
+++ rebuild-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+green='\e[0;32m'
+red='\e[0;31m'
+neutral='\e[0;m'
+
+clear
+echo -e "${red}\t\t[Deleting Docker data]\n${neutral}"
+yes | docker system prune -a
+yes | docker volume prune
+clear
+echo -e "${green}\t\t[Run docker-compose up --build]\n${neutral}"
+docker-compose build -q
+clear
